65930eb1563749791c7fc009eeff3235ca508ad8
Jeremy Springer <Jeremyspringer50@gmail.com>
2016-11-03 22:41:50 -0400

    finished internal

---
+++ src/js/main.js
@@ -0,0 +1,43 @@
+import $ from 'jquery';
+
+console.log("load");
+var list = $(".list")
+
+var jax = $.ajax({
+	url:'https://randomuser.me/api/?results=12',
+	dataType:'json',
+	success: listForm
+
+});
+console.log(listForm)
+
+
+
+function listForm (object) {
+	for(var i=0; i < object.results.length; i++){
+		var result = object.results[i];
+		var listHTML = listTemplate(result);
+		list.append(listHTML);
+	}
+	console.log(object)
+}
+
+// listTemplate()
+
+function listTemplate (prop){
+var HTML=`
+<div class= "format">
+<img src="${prop.picture.large}" class="picture">
+<div class= "name">${prop.name.first}${prop.name.last}</div>
+<div class="email">${prop.email}</div>
+<div class="street">${prop.location.street}</div>
+<div class="number">${prop.cell}</div>
+
+
+</div>
+`;
+
+
+return HTML;
+
+}
